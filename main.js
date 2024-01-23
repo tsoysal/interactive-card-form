bfd26615e143ea673553feb1fd9aadc9731c56d4
Tahaberk SOYSAL <berksysl.g@gmail.com>
2024-01-24 02:10:12 +0300

    add validation to inputs

--- main.js
+++ main.js
@@ -13,30 +13,45 @@ const cardYearInput = document.querySelector('input[name="year"]');
 const cardCvcInput = document.querySelector('input[name="cvc"]');
 
 cardHolderInput.addEventListener('input', (e) => {
-    cardHolder.innerText = e.target.value;
     if (e.target.value == "") {
         cardHolder.innerText = "JANE APPLESEED";
     }
+    if(checkLetters(e.target.value)) {
+    cardHolder.innerText = e.target.value;
+    }
+    else {
+        e.target.value = e.target.value.slice(0, e.target.value.length-1)
+    }
 })
 cardNoInput.addEventListener('input', (e) => {
-    cardNo.innerText = e.target.value;
+    //format card number groups
+    e.target.value = e.target.value.replace(/(\d{4})(?=\d)/g, '$1 ');
     if (e.target.value == "") {
         cardNo.innerText = "0000 0000 0000 0000";
     }
+    else if (e.target.value.length > 19) {
+        e.target.value = e.target.value.slice(0, 19)
+    }
+    else {
+        cardNo.innerText = e.target.value;
+    }
 })
 cardMonthInput.addEventListener('input', (e) => {
+    e.target.value = e.target.value.slice(0, 2);
     cardMonth.innerText = e.target.value;
     if (e.target.value == "") {
         cardMonth.innerText = "00";
     }
 })
 cardYearInput.addEventListener('input', (e) => {
+    e.target.value = e.target.value.slice(0, 2);
     cardYear.innerText = e.target.value;
     if (e.target.value == "") {
         cardYear.innerText = "00";
     }
 })
 cardCvcInput.addEventListener('input', (e) => {
+    e.target.value = e.target.value.slice(0, 3);
     cardCvc.innerText = e.target.value;
     if (e.target.value == "") {
         cardCvc.innerText = "000";
@@ -44,7 +59,7 @@ cardCvcInput.addEventListener('input', (e) => {
 });
 
 function checkLetters(val) {
-    const letters = /^[A-Za-z]+$/;
+    const letters = /^[A-Za-z\s]+$/;
     if (val.match(letters)) {
         return true;
     } else {
